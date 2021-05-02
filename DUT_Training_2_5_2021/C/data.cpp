df781285c26640d500c2f3c693ca4a0e96a56a95
nvatuan <24392632+nvatuan@users.noreply.github.com>
2021-05-02 23:45:37 +0700

    Added team's solution for B, C

--- DUT_Training_2_5_2021/C/data.cpp
+++ DUT_Training_2_5_2021/C/data.cpp
@@ -1,42 +1,58 @@
 #include <bits/stdc++.h>
-
-#define debug(x) cout << #x << " = " << x << endl;
-#define debugarr2d(x, n, m)                                                    \
-  {                                                                            \
-    for (int _ = 0; _ < n; ++_) {                                              \
-      for (int __ = 0; __ < m; ++__)                                           \
-        cout << x[_][__] << " ";                                               \
-      cout << endl;                                                            \
-    }                                                                          \
-    cout << endl;                                                              \
-  }
-#define debugarr(x, n)                                                         \
-  {                                                                            \
-    for (int _ = 0; _ < n; ++_)                                                \
-      cout << x[_] << " ";                                                     \
-    cout << endl;                                                              \
-  }
-#define debugvi(x)                                                             \
-  {                                                                            \
-    for (int _ = 0; _ < x.size(); ++_)                                         \
-      cout << x[_] << " ";                                                     \
-    cout << endl;                                                              \
-  }
-
-#define fi first
-#define se second
-#define pb push_back
 using namespace std;
 
-typedef int64_t ll;
-typedef vector<int> vi;
-typedef pair<int, int> ii;
+// struct Hasher {
+//     size_t operator()(const vector<int>& rhs) const {
+//         string id = "";
+//         for (int i=0; i<26; i++) {
+//             // cerr << p.first << ' ' << p.second << '\n';
+//             id += to_string(rhs[i]) + ";";
+//         }
+//         // cerr << id << '\n';
+//         return hash<string>{}(id);
+//     }
+// };
+
+string a, b;
+
+void solve() {
+    set<vector<int>> histogram_set;
+    cin >> a >> b;
+    swap(a, b);
+
+    int n = a.length();
+    for (int i=0; i<n; i++) {
+        vector<int> histogram (26, 0);
+        for (int j=i; j<n; j++) {
+            histogram[a[j]-'a']++;
+            histogram_set.insert(histogram);
+        }
+    }
+
+    int l=0, r=-1;
+    for (int i=0; i<n; i++) {
+        vector<int> histogram (26, 0);
+        for (int j=i; j<n; j++) {
+            histogram[b[j]-'a']++;
+            if (histogram_set.find(histogram) != histogram_set.end()) {
+                if (j-i+1 > r-l+1) {
+                    l = i;
+                    r = j;
+                }
+            }
+        }
+    }
+    if (l <= r) {
+        cout << b.substr(l, r-l+1) << '\n';
+    } else {
+        cout << "NONE\n";
+    }
+}
 
 int main() {
-#ifdef LOCAL
-  freopen("data.inp", "r", stdin);
-  freopen("data.out", "w", stdout);
-#endif
-
-  return 0;
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--) solve();
 }
