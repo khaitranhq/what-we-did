078d226c18b56ad24bf848856ea1f8aba2a97231
lioaslan <arey.tieuleo.5.7@gmail.com>
2021-05-08 22:10:11 +0700

    Add G

--- DUTPC_2021_Round_2/G/data.cpp
+++ DUTPC_2021_Round_2/G/data.cpp
@@ -32,11 +32,42 @@ typedef int64_t ll;
 typedef vector<int> vi;
 typedef pair<int, int> ii;
 
+const int MAX = 1e5 + 5;
+int n, A, B;
+int a[MAX];
+
 int main() {
 #ifdef LOCAL
   freopen("data.inp", "r", stdin);
   freopen("data.out", "w", stdout);
 #endif
+
+  cin >> n >> A >> B;
+  map<int, int> mp;
+  for (int i = 0; i < n; ++i) {
+    cin >> a[i];
+    if (mp.count(A - a[i]) && mp[A - a[i]] == -1) {
+      mp[a[i]] = 0;
+      mp[A - a[i]] = 0;
+    } else if (mp.count(B - a[i]) && mp[B - a[i]] == -1) {
+      mp[a[i]] = 1;
+      mp[B - a[i]] = 1;
+    } else
+      mp[a[i]] = -1;
+  }
+
+  vector<int> ans;
+  for (int i = 0; i < n; ++i)
+    if (mp[a[i]] != -1)
+      ans.push_back(mp[a[i]]);
+    else {
+      cout << -1;
+      return 0;
+    }
+
+  cout << "YES" << endl;
+  for (int i = 0; i < ans.size(); ++i)
+    cout << ans[i] << " ";
 
   return 0;
 }
