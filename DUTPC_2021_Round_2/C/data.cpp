8a1a8b01a4ce2685b2190d45cc4fb6812c05d464
lioaslan <arey.tieuleo.5.7@gmail.com>
2021-05-08 22:10:51 +0700

    Add C

--- DUTPC_2021_Round_2/C/data.cpp
+++ DUTPC_2021_Round_2/C/data.cpp
@@ -32,11 +32,42 @@ typedef int64_t ll;
 typedef vector<int> vi;
 typedef pair<int, int> ii;
 
+const int MAX = 1e6 + 5;
+const ll BASE = 998244353;
+int n;
+ll a[MAX];
+
+ll power(ll x, ll y) {
+  if (!y)
+    return 1;
+  ll tmp = power(x, y / 2);
+  if (y % 2)
+    return (tmp * tmp % BASE * x) % BASE;
+  return tmp * tmp % BASE;
+}
+
 int main() {
 #ifdef LOCAL
   freopen("data.inp", "r", stdin);
   freopen("data.out", "w", stdout);
 #endif
+
+  cin >> n;
+  for (int i = 0; i < n; ++i)
+    cin >> a[i];
+
+  int beforePower = n - 1;
+  ll sum = power(2, n - 1) * a[0] % BASE, prefixSum = 0;
+  for (int i = 1; i < n; ++i) {
+    ll tmp1 = power(2, beforePower - 1) * a[i - 1] % BASE;
+    ll tmp2 = power(2, beforePower - 1) * a[i] % BASE;
+    prefixSum = (prefixSum + tmp1) % BASE;
+    sum = (sum + prefixSum + tmp2) % BASE;
+
+    --beforePower;
+  }
+
+  cout << sum;
 
   return 0;
 }
