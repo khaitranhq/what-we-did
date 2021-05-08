0880dca79297cbe0a5c4eb2b62592f327ee43828
khoa8700 <74928334+khoa8700@users.noreply.github.com>
2021-05-08 22:15:54 +0700

    add F

--- DUTPC_2021_Round_2/F/data.cpp
+++ DUTPC_2021_Round_2/F/data.cpp
@@ -1,42 +1,23 @@
 #include <bits/stdc++.h>
-
-#define debug(x) cout << #x << " = " << x << endl;
-#define debugarr2d(x, n, m)                                                    \
-  {                                                                            \
-    for (int _ = 0; _ < n; ++_) {                                              \
-      for (int __ = 0; __ < m; ++__)                                           \
-        cout << x[_][__] << " ";                                               \
-      cout << endl;                                                            \
-    }                                                                          \
-    cout << endl;                                                              \
-  }
-#define debugarr(x, n)                                                         \
-  {                                                                            \
-    for (int _ = 0; _ < n; ++_)                                                \
-      cout << x[_] << " ";                                                     \
-    cout << endl;                                                              \
-  }
-#define debugvi(x)                                                             \
-  {                                                                            \
-    for (int _ = 0; _ < x.size(); ++_)                                         \
-      cout << x[_] << " ";                                                     \
-    cout << endl;                                                              \
-  }
-
-#define fi first
-#define se second
-#define pb push_back
 using namespace std;
-
-typedef int64_t ll;
-typedef vector<int> vi;
-typedef pair<int, int> ii;
-
-int main() {
-#ifdef LOCAL
-  freopen("data.inp", "r", stdin);
-  freopen("data.out", "w", stdout);
-#endif
-
-  return 0;
+int main()
+{
+    freopen("data.inp", "r", stdin);
+    freopen("data.out", "w", stdout);
+    string a, b;
+    long long c;
+    int n;
+    cin >> n;
+    map<string, long long> cnt;
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> a >> b >> c;
+        cnt[a] += c;
+        cnt[b] += c;
+    }
+    for (auto i : cnt)
+    {
+        cout << i.first << ' ' << i.second << endl;
+    }
+    return 0;
 }
